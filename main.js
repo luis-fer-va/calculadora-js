811128e20a606cb31e70b339eac5449d34effcb5
luis <luisaovallejo@gmail.com>
2021-10-14 00:27:30 -0500

    Calculadora con JS terminada

---
+++ main.js
@@ -0,0 +1,9 @@
+function valueButton(e){
+    calculatorform.screen.value += e.value;
+}
+function Clear(){
+    calculatorform.screen.value=null;
+}
+function calculate(){
+    calculatorform.screen.value = eval(calculatorform.screen.value);
+}
